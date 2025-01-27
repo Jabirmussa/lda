8dd35ffbfb68bacb424d61b4180a65aaa5819e04
Growskills <jabirmussa185@gmail.com>
2025-01-27 11:02:52 +0200

    refactor: integrate GSAP and GLightbox for enhanced animations and gallery functionality

--- js/script.js
+++ js/script.js
@@ -12,9 +12,63 @@ function fixHeaderOnScroll() {
 window.addEventListener('scroll', fixHeaderOnScroll);
 fixHeaderOnScroll();
 
+if(document.querySelector('.galery')) {
+   
+    const lightbox = GLightbox({
+        selector: '.galery .wrapper a',
+        touchNavigation: true,
+        loop: true
+    });
+}
 
-const lightbox = GLightbox({
-    selector: '.galery .wrapper a',
-    touchNavigation: true,
-    loop: true
+//gsap
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+    smooth: 2, 
+    effects: true
 });
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: 'footer',
+      start: '-100% bottom',
+      end: 'bottom top',
+      scrub: true
+    },
+    defaults: {ease: 'none'}
+  })
+
+  .to('.footer-title', {
+    right: '31%'
+  })
+const tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.projects',
+      start: 'top bottom',
+      end: '20% top',
+      scrub: true
+    },
+})
+
+.to('.project-item', {
+    scale: 1
+})
+
+tl3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.overview',
+    //   start: 'top top',
+      end: '20% top',
+      scrub: 1.2,
+    markers: true
+    },
+    duration: 0.3
+    
+})
+tl3.to(".overview-item-image", { 
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", 
+    scale:1.1, 
+    ease: "power1.out"
+ });
